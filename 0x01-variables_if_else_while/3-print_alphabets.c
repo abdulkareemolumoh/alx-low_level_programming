fdf3814fa765e360167b9eac431845dbd29aae79
abdulkareemolumoh <abdulkareemolumoh@protonmail.com>
2023-03-17 15:47:21 -0700

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return 0
+ */
+
+int main(void)
+{
+char x, y;
+
+for (x = 'a', y = 'A'; x <= 'z' && y <= 'Z'; x++, y++)
+{
+	putchar(x);
+	putchar(y);
+	putchar('\n');
+}
+		return (0);
+}
