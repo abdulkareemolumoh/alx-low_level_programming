b791c6bcde8e5990cc0d127b0e11ba82a0aa1511
abdulkareemolumoh <abdulkareemolumoh@protonmail.com>
2023-03-18 08:20:03 -0700

    The 101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,20 +8,22 @@
 int main(void)
 {
 	int i, j, k;
+
 	for (i = 0; i < 9; i++)
 	{
-		for (j = i; j < 9; j++)
+		for (j = i + 1; j < 9; j++)
 		{
-			for (k = j; k < 9; k++)
+			for (k = j + 1; k <= 9; k++)
 			{
-				putchar(i + 0);
-				putchar(j + 0);
-				putchar(k + 0);
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+
+				if (i != 7 || j != 8 || k != 9)
+				{
+			putchar(',');
+			putchar(' ');
 			}
-			if (i != 7 && j != 8 && k != 9)
-			{
-				putchar(',');
-				putchar(' ');
 			}
 		}
 	}
