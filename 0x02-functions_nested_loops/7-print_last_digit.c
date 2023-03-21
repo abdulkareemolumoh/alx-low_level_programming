7341f2d021b8d50f94319da1853b8c2bd9022917
abdulkareemolumoh <abdulkareemolumoh@protonmail.com>
2023-03-21 12:59:20 -0700

    fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,17 +6,11 @@
  *
  * Return: The value of the last digit.
  */
-int print_last_digit(int n)
-{
-	int last_digit;
-
-	if (n < 0)
-		n = -n;
-
-	last_digit = n % 10;
-
-	_putchar(last_digit + '0');
-
-	return (last_digit);
+int print_last_digit(int num) {
+	int last_digit = num % 10;
+	if (last_digit < 0) {
+		last_digit *= -1;
+	}
+	_putchar('0' + last_digit);
+	return last_digit;
 }
-
