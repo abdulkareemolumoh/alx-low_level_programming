5c4e15ed5400d43e39739ae8a29a6d76fda04d59
abdulkareemolumoh <abdulkareemolumoh@protonmail.com>
2023-03-21 10:26:50 -0700

    main

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry Point
+ *
+ * Return : 0
+ */
+
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
