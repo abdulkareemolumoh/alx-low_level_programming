c98fc0a29cab4c8d7d8327f65704c525524b381d
abdulkareemolumoh <abdulkareemolumoh@protonmail.com>
2023-03-27 07:17:04 -0700

    Fix betty

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -11,10 +11,9 @@ void _puts(char *str)
 {
 	int i;
 
-	for
-		(i = 0; str[i] != '\0'; i++)
-		{
-			_putchar(str[i]);
-		}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
 	_putchar('\n');
 }
