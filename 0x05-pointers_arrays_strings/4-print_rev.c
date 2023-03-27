9e78c883d48f6495657b470118b80bd33e947753
abdulkareemolumoh <abdulkareemolumoh@protonmail.com>
2023-03-27 09:02:02 -0700

    update

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev -  function that prints a string, in reverse
+ * @s: integer
+ *
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+	int i;
+
+	while (*(s + len) != '\0')
+		len++;
+
+			for (i = len - 1; i >= 0; i--)
+				_putchar(*(s + i));
+	_putchar('\n');
+}
