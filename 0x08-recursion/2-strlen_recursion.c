7b37f5cbd25739a45dd8d0fdb69cf5d229ea9c40
abdulkareemolumoh <abdulkareemolumoh@protonmail.com>
2023-04-05 04:01:04 -0700

    String Lenght

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns the lenght of a string.
+ * @s: String
+ *
+ * Return: Returns lenght of a string.
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (_strlen_recursion(s + 1) + 1);
+	}
+
+}
