c46f77bf6331073785b5fea0fd8c3ea31d5f6f17
abdulkareemolumoh <abdulkareemolumoh@protonmail.com>
2023-04-09 13:57:57 -0700

    argc

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - entry point
+ * @argc: argument count.
+ * @argv: argument vector
+ *
+ * Return: zero
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
