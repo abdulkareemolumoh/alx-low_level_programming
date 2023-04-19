09a5ec35f1672422b92823092ba78ad866ff8c13
abdulkareemolumoh <abdulkareemolumoh@protonmail.com>
2023-04-19 15:28:13 -0700

    ptr

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,33 @@
+int op_add(int a, int b)
+{
+	return a + b;
+}
+
+int op_sub(int a, int b)
+{
+	return a - b;
+}
+
+int op_mul(int a, int b)
+{
+	return a * b;
+}
+
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		return -1;
+	}
+	return a / b;
+}
+
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		return -1;
+	}
+	return a % b;
+}
+
