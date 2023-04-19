fe755d040d96b36a564ede3f659b36d4956b205f
abdulkareemolumoh <abdulkareemolumoh@protonmail.com>
2023-04-19 04:39:02 -0700

    fptr

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name as is
+ * @name: name of the person
+ * @f: function
+ *
+ * Return: Nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	int i = 0;
+
+	f(&(name[i]));
+
+}
