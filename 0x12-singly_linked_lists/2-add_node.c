f64ecf919421627a0a3999958809cde7cc68826c
abdulkareemolumoh <abdulkareemolumoh@protonmail.com>
2023-04-27 17:39:43 -0700

    add

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * add_node -  function that adds a new node at the beginning of a list_t list
+ * @head: Argument
+ * @str: String Argument
+ *
+ * Return: Adrress of the new element || null
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
