4bdc3a240d917f672ed5802d6f7e696839a6a72b
abdulkareemolumoh <abdulkareemolumoh@protonmail.com>
2023-05-02 02:35:19 -0700

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * listint_len - function that returns the number of elements in a linked list
+ * @h: pointer to header
+ *
+ * Return: number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+
+	return (i);
+}
