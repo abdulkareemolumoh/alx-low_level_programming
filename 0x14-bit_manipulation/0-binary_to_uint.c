8b15a835a03eaf7d8bbd75a0f96d4d9d9cf99167
abdulkareemolumoh <abdulkareemolumoh@protonmail.com>
2023-05-04 07:13:09 -0700

    fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,32 +8,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	size_t len = strlen(b);
-	unsigned int result = 0;
-	size_t i;
+	unsigned int num = 0;
 
-	if (b == NULL)
-	{
+	if (!b)
 		return (0);
-	}
 
-	for (i = 0; i < len; i++)
+	while (*b)
 	{
-		char c = b[i];
-
-		if (c == '0')
-		{
-			result = (result << 1) + 0;
-		}
-		else if (c == '1')
-		{
-			result = (result << 1) + 1;
-		}
+		if (*b == '0' || *b == '1')
+			num = (num << 1) + (*b++ - '0');
 		else
-		{
 			return (0);
-		}
 	}
 
-	return (result);
+	return (num);
 }
